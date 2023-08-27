041220c6e2edc459222b3fd48f1c473c62b82722
Miraj Maharjan <83446723+MirajMjn@users.noreply.github.com>
2023-08-27 20:17:49 +0545

    last update i guess unless bugs found

--- LAB_4_SQLQuery.sql
+++ LAB_4_SQLQuery.sql
@@ -12,15 +12,39 @@ ORDER BY [name] ASC;
 SELECT TOP 3 * FROM teacher;
 
 -- to display sum of all salaries of employee
-SELECT SUM(salary) FROM employee;
+SELECT SUM(salary) AS Total FROM employee;
 
 --to dsiplay min salary
 SELECT MIN(salary) FROM employee;
 
--- 6
+-- Display the average price of book written by same author
 SELECT author, AVG(price) AS average FROM book
 GROUP BY author;
 
--- 7
+-- Display publication name and number of books published 
+-- by it from book list relation publication wise.
+
 SELECT publication, COUNT(*) FROM booklist
 GROUP BY publication;
+
+-- Display the bid and bname of books whose price is 
+-- greater than average prices of book
+UPDATE book SET price = 200 WHERE bid = 8;
+SELECT * FROM book;
+
+SELECT bid, bname FROM book
+WHERE price > (SELECT AVG(price) FROM book);
+
+-- Find the bid , bname and author in ascending order where author name is started by “s”.
+SELECT bid, bname, author FROM book
+WHERE author LIKE 'J%';
+
+--Find the teacher name and book taken by him. The teacher’s salary who takes the 
+--book should be the maximum salary.
+
+
+--Find the authors name who have written more than one book.
+SELECT * FROM book;
+
+SELECT author FROM book
+GROUP BY author HAVING COUNT(author) > 1;
